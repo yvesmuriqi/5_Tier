b3c880eea24fffbab3b9a337e6cb23ad6669d09f
flxsch <43772935+flxsch@users.noreply.github.com>
2020-09-15 11:23:28 +0200

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,18 +1,18 @@
 create TABLE personaldata(
-last_name varchar(255) not null,
 first_name varchar(255) not null,
-birthday date not null,
+last+_name varchar(255) not null,
+birth_date not null,
 e-mail varchar(255),
-ahv_number varchar(255) not null,
-personal_number int not null unique,
+ahv varchar(255) not null,
+id_employee int not null unique,
 telephone varchar(9),
-primary key (personal_number)
+primary key (employee_id)
 );
     
 create table company_data(
-id_personal_number int, 
+employee_id int, 
 department varchar(50) not null,
 job_title varchar(50) not null,
 job_description varchar(255),
-foreign key (id_personal_number) REFERENCES personaldata(personal_number) ON DELETE CASCADE ON UPDATE CASCADE
+foreign key (employee_id) REFERENCES personaldata(id_employee) ON DELETE CASCADE ON UPDATE CASCADE
 );
