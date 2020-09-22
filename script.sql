f381eb14bc6df1be4c2c5a9cbe0fa91128c67b69
yvesmuriqi <49523629+yvesmuriqi@users.noreply.github.com>
2020-09-22 18:25:23 +0200

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,7 +1,7 @@
 create TABLE personaldata(
 first_name varchar(255) not null,
 last_name varchar(255) not null,
-birth_date not null,
+birth_date date not null,
 e-mail varchar(255),
 ahv varchar(255) not null,
 id_employee int not null unique,
